b0102368adb8c7478e43a7441e8cf88be5296cce
Ezzy Elliott <ezzy.elliott@gmail.com>
2015-11-01 12:21:11 +0000

    Refactored to use Forwardable

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,24 @@
+require 'forwardable'
+
 class Game
+
+  extend Forwardable
   attr_reader :message, :user, :user2
+
+  def_delegator :user, :name, :user_name
+  def_delegator :user, :email, :user_email
+  def_delegator :user, :results, :user_results
+  def_delegator :user, :pick, :user_pick
+  def_delegator :user, :score, :user_score
+  def_delegator :user, :played, :user_played
+
+  def_delegator :user2, :name, :user2_name
+  def_delegator :user2, :email, :user2_email
+  def_delegator :user2, :results, :user2_results
+  def_delegator :user2, :pick, :user2_pick
+  def_delegator :user2, :score, :user2_score
+  def_delegator :user2, :played, :user2_played
+
 
   def initialize(*users)
     @message = "Register or login before playing"
@@ -7,18 +26,6 @@ class Game
     (@user2 = users.last) unless users.length < 2
     @message = "Hi #{@user.name}, click play to start game" unless users.empty?
   end
-
-  def winner
-    return user if user.pick == :rock && user2.pick == :scissors
-    return user if user.pick == :scissors && user2.pick == :paper
-    return user if user.pick == :paper && user2.pick == :rock
-    user2
-  end
-
-  def loser
-    winner == user ? user2 : user
-  end
-
 
   def update_score
     user.played +=1
@@ -34,6 +41,17 @@ class Game
   end
 
   private
+
+  def winner
+    return user if user.pick == :rock && user2.pick == :scissors
+    return user if user.pick == :scissors && user2.pick == :paper
+    return user if user.pick == :paper && user2.pick == :rock
+    user2
+  end
+
+  def loser
+    winner == user ? user2 : user
+  end
 
   attr_writer :message, :user
 
